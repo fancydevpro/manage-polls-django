4ec9332daa42ef912548838bb53d7a300fed51a1
fancydevpro <borisglab@gmail.com>
2018-10-01 06:12:41 +0300

    update results view

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .models import Question, Choice
 # from django.template import loader
-from django.http import Http404
+from django.urls import reverse
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -21,8 +21,8 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-  response = 'You\'re looking at the results of question %s.'
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
